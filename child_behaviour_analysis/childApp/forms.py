9d17ad53a722e2f28e3366d7c8d9a14cb0cb99aa
Pavi <pavithiranagarajan@gmail.com>
2025-04-17 13:58:04 +0530

    Add files via upload

---
+++ child_behaviour_analysis/childApp/forms.py
@@ -0,0 +1,38 @@
+# from django import forms
+# from .models import *
+#
+#
+# class childInfoForm(forms.ModelForm):
+#     class Meta():
+#         model = childModel
+#         fields = '__all__'
+#         widgets = {
+#             'Mood': forms.Select(attrs={'class': 'form-control'}),
+#             'Screen_Time': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'Study_Hours': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'Meals_Per_Day': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'Sleep_Hours': forms.NumberInput(attrs={'class': 'form-control'}),
+#         }
+#         # fields = ['Mood', 'Screen_Time', 'Study_Hours', 'Meals_Per_Day', 'Sleep_Hours']
+
+
+from django import forms
+from .models import *
+
+class childInfoForm(forms.ModelForm):
+    class Meta:
+        model = childModel
+        fields = '__all__'
+        widgets = {
+            # 'mood': forms.Select(attrs={'class': 'form-control'}),
+            'Play_Hours': forms.NumberInput(attrs={'class': 'form-control'}),
+            'Screen_Time': forms.NumberInput(attrs={'class': 'form-control'}),
+            'Study_Hours': forms.NumberInput(attrs={'class': 'form-control'}),
+            'Meals_Per_Day': forms.NumberInput(attrs={'class': 'form-control'}),
+            'Sleep_Hours': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(childInfoForm, self).__init__(*args, **kwargs)
+        # Replace '----' with 'Select Mood'
+        # self.fields['Mood'].choices = [('', 'Select Mood')] + list(self.fields['Mood'].choices)[1:]
---
+++ child_behaviour_analysis/childApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class childModel(models.Model):
+    # MOOD_CHOICES = [
+    #     ('Happy', 'Happy'),
+    #     ('Sad', 'Sad'),
+    #     ('Neutral', 'Neutral'),
+    # ]
+    # Mood = models.CharField(max_length=20, choices=MOOD_CHOICES)
+    # Blood_Urea=models.FloatField()
+    # Serum_Creatine=models.FloatField()
+    # Packed_cell_volume=models.FloatField()
+    # White_blood_count=models.FloatField()
+    # Age = models.FloatField()
+    Play_Hours = models.FloatField()
+    Screen_Time = models.FloatField()
+    Study_Hours = models.FloatField()
+    Meals_Per_Day = models.FloatField()
+    Sleep_Hours = models.FloatField()
